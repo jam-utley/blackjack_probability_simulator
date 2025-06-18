0fe1c49b2b95d0ba6ef1e70a695a5832cc371fdc
James Utley <james.utley000@gmail.com>
2025-06-18 11:08:09 -0400

    added anchored window in top right

--- main.rs
+++ main.rs
@@ -1,9 +1,8 @@
 use eframe::egui::{self, ComboBox};
 use eframe::{run_native, App, CreationContext, NativeOptions};
 
-//things to do: 
+//things to do:
 //assign cards to a dealer vs player
-//move the text to specific places of the screen
 
 struct BlackjackAid {
     player: Vec<String>, //Picks between player and the dealer
@@ -52,50 +51,66 @@ impl Default for BlackjackAid {
 impl App for BlackjackAid {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let visuals = egui::Visuals {
-            window_fill: egui::Color32::from_rgb(10, 10, 40), // dark gray background
-            ..egui::Visuals::dark() // start from dark theme and customize
+            //sets background ccolor for dropown menus and windows, not the entire page
+            window_fill: egui::Color32::from_rgb(10, 10, 40),
+            ..egui::Visuals::dark() //Starts from dark theme
         };
 
         ctx.set_visuals(visuals);
-        
-        egui::CentralPanel::default().frame(egui::Frame::default().fill(egui::Color32::from_rgb(0,150,0))).show(ctx, |ui| {
-            ui.label("Choose a card:");
-            ComboBox::from_label("Player/Dealer")
-                .selected_text(&self.selected_player)
-                .show_ui(ui, |ui| {
-                    for player in &self.player {
-                        ui.selectable_value(&mut self.selected_player, player.clone(), player);
-                    }
-                });
-            ComboBox::from_label("Suit")
-                .selected_text(&self.selected_suit)
-                .show_ui(ui, |ui| {
-                    for suit in &self.suit {
-                        ui.selectable_value(&mut self.selected_suit, suit.clone(), suit);
-                    }
-                });
-            ComboBox::from_label("Number")
-                .selected_text(&self.selected_number)
-                .show_ui(ui, |ui| {
-                    for card_number in &self.card_number {
-                        ui.selectable_value(
-                            &mut self.selected_number,
-                            card_number.clone(),
-                            card_number,
-                        );
-                    }
-                });
-            if ui.button("Add").clicked() {
-                //appends selected number and suit to a rolling string of values
-                self.recorded_cards +=
-                    &format!("the {} of {}\n", self.selected_number, self.selected_suit)
-                        .to_string();
-            }
-            ui.separator();
 
-            ui.label(format!("You selected:"));
-            ui.label(egui::RichText::new(format!("{}", self.recorded_cards)).color(egui::Color32::BLUE));
+        
+        //creates floating window. anchored at top right, offset of -5.0,5.0
+        egui::Window::new("My Window").anchor(egui::Align2::RIGHT_TOP, [-5.0,5.0]).show(ctx, |ui| { 
+            ui.label(
+                    egui::RichText::new(format!("{}", self.recorded_cards))
+                        .color(egui::Color32::BLUE),
+                );;
         });
+
+        egui::CentralPanel::default()
+            .frame(egui::Frame::default().fill(egui::Color32::from_rgb(0, 150, 0))) //sets page background color
+            .show(ctx, |ui| {
+                ui.label("Choose a card:");
+                ComboBox::from_label("Player/Dealer")
+                    .selected_text(&self.selected_player)
+                    .show_ui(ui, |ui| {
+                        for player in &self.player {
+                            ui.selectable_value(&mut self.selected_player, player.clone(), player);
+                        }
+                    });
+                ComboBox::from_label("Suit")
+                    .selected_text(&self.selected_suit)
+                    .show_ui(ui, |ui| {
+                        for suit in &self.suit {
+                            ui.selectable_value(&mut self.selected_suit, suit.clone(), suit);
+                        }
+                    });
+                ComboBox::from_label("Number")
+                    .selected_text(&self.selected_number)
+                    .show_ui(ui, |ui| {
+                        for card_number in &self.card_number {
+                            ui.selectable_value(
+                                &mut self.selected_number,
+                                card_number.clone(),
+                                card_number,
+                            );
+                        }
+                    });
+                if ui.button("Add").clicked() {
+                    //appends selected number and suit to a rolling string of values
+                    self.recorded_cards +=
+                        &format!("the {} of {}\n", self.selected_number, self.selected_suit)
+                            .to_string();
+                }
+                ui.separator();
+
+                ui.label(format!("You selected:"));
+                //sets this text color different
+                ui.label(
+                    egui::RichText::new(format!("{}", self.recorded_cards))
+                        .color(egui::Color32::BLUE),
+                );
+            });
     }
 }
 
